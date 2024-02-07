f3078d5816972d0250a1c2355cc310a36f20ba06
anagha-972000 <anakhap6@gmail.com>
2024-02-07 13:54:20 +0530

    First commit

---
+++ shop/context_processors.py
@@ -0,0 +1,44 @@
+from .models import Category,Product
+def menu_links(request):
+    links=Category.objects.all()
+    return dict(links=links)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
